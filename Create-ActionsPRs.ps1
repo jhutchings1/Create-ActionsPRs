9ca0652f10e5126a7f5f51b05088e5ed1106d85d
Justin Hutchings <jhutchings1@users.noreply.github.com>
2020-04-24 10:45:57 -0700

    Clean up methods/documentation

---
+++ Create-ActionsPRs.ps1
@@ -0,0 +1,143 @@
+
+function CreatePullRequestsFromFile {
+  param (
+      [string]
+      $FileName,
+      [string] $CommitMessage,
+      [string] $PRBody
+  )
+  $repos = gc ./repos.txt
+
+  # Clone repos
+  foreach ($repo in $repos) 
+  {
+    cd $PSScriptRoot
+    $chunks = $repo.split("/")
+    $repo_nwo = $chunks[3] + "/" + $chunks[4]
+    git clone $repo $repo_nwo
+    cd $repo_nwo
+    git checkout -b codeql
+    if ((Get-ChildItem .github -ErrorAction SilentlyContinue).Count -eq 0) {
+      mkdir .github
+    }
+    if ((Get-ChildItem .github/workflows -ErrorAction SilentlyContinue).Count  -eq 0) {
+      mkdir .github/workflows
+    }
+    copy-item "../../codeql.yml" -destination ".github/workflows/"
+    git add .github/workflows/codeql.yml
+    git commit -a -m $CommitMessage
+    gh pr create --body $PRBody --title $CommitMessage
+  }
+}
+
+function CreatePullRequestForRepositories {
+  param (
+      # An Array of Repository objects as returned by the GitHub API
+      [array] $Repositories,
+      [string] $CommitMessage,
+      [string] $PRBody
+  )
+  foreach ($repo in $repos) 
+  {
+    cd $PSScriptRoot
+
+    git clone $repo.git_url $repo.full_name
+
+    cd $repo.full_name
+    git checkout -b codeql
+
+    copy-item "$PSScriptRoot/workflows" -Destination ".github/workflows/" -Recurse
+    git add -A
+    git commit -a -m $CommitMessage
+    gh pr create --body $PRBody --title $CommitMessage
+  }
+}
+
+function getAuthenticationToken {
+  $token = Get-ChildItem Env:\GITHUB_TOKEN -ErrorAction SilentlyContinue
+  if ($token -eq $null) {
+    $envFile = Get-Content .env 
+    foreach ($line in $envFile) {
+      if ($line.startsWith("GITHUB_TOKEN=")) {
+        $token = $line.Substring($line.IndexOf("=") + 1)
+      }
+    }
+  }
+  return $token
+}
+
+function getHeaders {
+  $token = getAuthenticationToken
+  $authheader = "Bearer " + $token
+  $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+  $headers.Add("Authorization",$authheader)
+  return $headers
+}
+  
+function GetReposFromOrganization {
+  param (
+      [string] $Organization
+  )
+  $url = "https://api.github.com/orgs/$Organization/repos"
+  $headers = getHeaders
+
+  $repos = Invoke-RestMethod -Uri $url -Method Get -Headers $headers -ResponseHeadersVariable 'response'
+
+  if ($response -ne $null) {
+    # Parse the Link header to paginate
+    $response.Link[0] -match '.*?page=([0-9]*)>; rel="last"'
+    $pages = [int]($Matches[1])
+
+    # Paginate
+    for ($page = 2; $page -lt $pages; $page++) {
+      $pageUrl = $url + "?page=" + $page 
+      $repos += Invoke-RestMethod -Uri $pageUrl -Method Get -Headers $headers
+    } 
+  }   
+
+  return $repos
+}
+
+function FilterForSupportedLanguages($repos) {
+  $languages = '"C"', '"C++"', '"Go"', '"C#"', '"Python"', '"Java"', '"JavaScript"', '"TypeScript"'
+  $filteredRepos = @()
+
+  foreach ($repo in $repos) {
+    if ($repo.GetType().Name -eq "PSCustomObject") {
+      $url = $repo.url + "/languages"
+      $headers = getHeaders
+
+      $langs = Invoke-WebRequest -Uri $url -Method Get -Headers $headers 
+      $value = $langs.Content
+
+      if ($value -ne "{}") {
+        if (ContainsAny -string $value -values $languages) {
+          $filteredRepos += $repo
+        }
+      }
+    }
+  }
+  return $filteredRepos
+}
+
+function ContainsAny {
+  param([string]$string, [array]$values)
+  foreach ($value in $values) {
+    if ($string.Contains($value)) {
+      return $true;
+    }
+  }
+  return $false;
+}
+
+function CreatePullRequestsForCodeQLLanguages {
+  param (
+      [string] $Organization,
+      [string] $CommitMessage = "Add CodeQL Analysis workflow",
+      [string] $PRBody = "Adds an Actions workflow which enables CodeQL analysis and will perform static analysis security testing on your code. You'll see results show up in pull requests and/or the Security tab. "
+  )
+  $repos = FilterForSupportedLanguages(GetReposFromOrganization -Organization $Organization);
+  CreatePullRequestForRepositories -Repositories $repos -CommitMessage $CommitMessage -PRBody $PRBody
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Create-ActionsPRs
 This repository creates pull requests to push a GitHub Actions workflow to a collection of workflows. 
+
+### Prerequisites
+* Install [PowerShell](https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell?view=powershell-7)
+* Install and configure [git](https://git-scm.com/), and configure your account for access to GitHub. [Configuring SSH Keys](https://help.github.com/en/github/authenticating-to-github/connecting-to-github-with-ssh)
+* Install [GitHub CLI](https://cli.github.com/)
+
+### Instructions
+
+#### Option 1: Use GitHub API to automatically target everything
+1. Clone this repository to your local machine
+2. Open powershell and navigate to the directory you cloned this into 
+3. Rename the file called `.env-example` to `.env` and add a [GitHub token](https://github.com/settings/tokens) with the `repo` scope. If your organization requires SSO, authorize the token for SSO. 
+4. Confirm that you have all of the workflow files you want in the `workflows` directory
+5. Run `./Create-ActionsPRs.ps1` to install the script. 
+6. Run `CreatePullRequestForRepositories -Repositories (GetReposFromOrganization -Organization orgname) -CommitMessage "message" -PRBody "prbody"` 
+
+This will create PRs in every repository that you have push permisisons in. 
+
+#### Option 2: Specify a list to target 
+1. Clone this repository to your local machine
+2. Open powershell and navigate to the directory you cloned this into 
+3. Confirm that you have all the workflow files you want in the `workflows` directory
+4. Run `./Create-ActionsPRs.ps1` to install the script. 
+5. Create a file with a list of repository URLs, with one repository per line. Save it somewhere you can access. 
+5. Run `CreatePullRequestsFromFile -FileName file.txt -CommitMessage "message" -PRBody "prbody"`. Replace file.txt with the path to your file, and update the CommitMessage and PRBody as appropriate. 
+
+#### Option 3: Choose all CodeQL eligible repositories
+1. Clone this repository to your local machine
+2. Open powershell and navigate to the directory you cloned this into 
+3. Rename the file called `.env-example` to `.env` and add a [GitHub token](https://github.com/settings/tokens) with the `repo` scope. If your organization requires SSO, authorize the token for SSO. 
+4. Confirm that you have all of the workflow files you want in the `workflows` directory
+5. Run `CreatePullRequestsForCodeQLLanguages -Organization orgname` with the organization you want to target instead of orgname. You may optionally override the commit message or PR body by adding `-CommitMessage` or `-PRBody` as appropriate.
